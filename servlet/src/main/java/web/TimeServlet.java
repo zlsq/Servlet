8eea251ec62efa1e2a532844dc778e50d8747d51
zlsq <shawn2012@163.com>
2016-10-13 21:47:12 +0800

    HTTP请求与响应
    增加http请求与响应的状态行，消息头，实体内容等。

--- servlet/src/main/java/web/TimeServlet.java
+++ servlet/src/main/java/web/TimeServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,10 +19,29 @@ public class TimeServlet extends HttpServlet {
 	private static final long serialVersionUID = 3060852870298693421L;
 
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse res) 
-		throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//1.使用request读取请求数据
+		//1.1读取请求行
+		System.out.println("请求方式" + req.getMethod());
+		System.out.println("访问路径" + req.getServletPath());
+		System.out.println("协议类型" + req.getProtocol());		
+		//1.2读取消息头
+		//getHeaderNames()返回key的迭代器,
+		//该迭代器是比Iterator更古老的迭代器.
+		Enumeration e = req.getHeaderNames();
+		while(e.hasMoreElements()){
+			String key = (String) e.nextElement();
+			String value = req.getHeader(key);
+			System.out.println(key + ":" + value);
+		}
+		//1.3读取实体内容
+		//本案例浏览器没有向服务器传递任何业务数据,所以实体内容为空
+		//2.使用response写响应数据
+		//2.1写状态行: 由服务器自动写
+		//2.2写消息头
 		//告诉浏览器给它输出的是什么格式的内容
 		res.setContentType("text/html");
+		
 		//获取输出流，该流指向的目标就是浏览器
 		PrintWriter out = res.getWriter();
 		//省略代码N行
@@ -29,11 +49,12 @@ public class TimeServlet extends HttpServlet {
 		SimpleDateFormat sdf = 
 			new SimpleDateFormat("HH:mm:ss");
 		String now = sdf.format(date);
+		//2.3写实体内容	
 		out.println("<!DOCTYPE HTML>");
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<title>TimeServlet</title>");
-		out.println("<meta charset=\"utf-8\">");
+		out.println("<meta charset='utf-8'>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<p>"+now+"</p>");
